0559cf2db27d9824025746139120ffd236723efe
jeffepereira <jefferson.pereira.nascimento10@gmail.com>
2022-03-29 22:50:40 -0300

    Create classes base ToDo

---
+++ app/Http/Controllers/ToDoController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ToDo;
+use Illuminate\Http\Request;
+
+class ToDoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ToDo  $toDo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ToDo $toDo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ToDo  $toDo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ToDo $toDo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ToDo  $toDo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ToDo $toDo)
+    {
+        //
+    }
+}
